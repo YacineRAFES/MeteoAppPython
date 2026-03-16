ecf5ad6956c68484a98227c519bbff2d623b1c15
Yacine RAFES <yacine.rafes@gmail.com>
2026-03-16 17:09:30 +0100

    Update main.py to integrate international cities and adjust layout; increase tempLabel font size in style.qss

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QWidget, QHBoxLayout
 from PySide6.QtCore import Qt
+from view.header.les_villes_internationales import villes_international
 
 
 # On définit une classe de fenêtre par héritage.
@@ -11,54 +12,15 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("MeteoApp")
-        self.resize(1280, 800)
+        self.resize(1600, 800)
 
         layout_main = QVBoxLayout()
         layout_main.setAlignment(Qt.AlignTop)
         layout_meteoInternational = QHBoxLayout()
 
-        for i in range(6):
-            bloc_ville = QWidget()
-            bloc_ville.setObjectName("bloc_ville")
+        for i in range(7):
 
-            bloc_vertical = QVBoxLayout()
-
-            # l'entete du bloc, on a l'icons de la météo et la température
-            layout_icons_temp = QHBoxLayout()
-            layout_icons_temp.setAlignment(Qt.AlignLeft)
-
-
-            icons = QLabel()
-            pixmap = QPixmap("img.png")
-            icons.setPixmap(pixmap)
-            icons.setPixmap(pixmap.scaled(100, 100))
-            layout_icons_temp.addWidget(icons)
-
-            temp = QLabel("20°C")
-            temp.setObjectName("tempLabel")
-            layout_icons_temp.addWidget(temp)
-
-            # un layout pour le nom de la ville et le code country (FR) (UK) etc..
-
-            layout_nomville_codecountry = QHBoxLayout()
-            layout_nomville_codecountry.setAlignment(Qt.AlignLeft)
-
-            nomville = QLabel("Nomville")
-            nomville.setObjectName("nomville")
-            layout_nomville_codecountry.addWidget(nomville)
-
-            code_country = QLabel("(FR)")
-            code_country.setObjectName("code_country")
-            layout_nomville_codecountry.addWidget(code_country)
-
-            bloc_vertical.addLayout(layout_nomville_codecountry)
-            bloc_vertical.addLayout(layout_icons_temp)
-
-            bloc_ville.setLayout(bloc_vertical)
-
-
-
-            layout_meteoInternational.addWidget(bloc_ville)
+            layout_meteoInternational.addWidget(villes_international(ville[i], code_country[i], temperature[i], temps[i]))
 
 
         layout_main.addLayout(layout_meteoInternational)
