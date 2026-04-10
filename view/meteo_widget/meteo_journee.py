df7a3a2b0b51bbf38541d13caf7133a59ccffd15
Yacine RAFES <yacine.rafes@gmail.com>
2026-04-10 10:59:12 +0200

    Mise en place de l'affichage pour la meteo dans la journée

--- view/meteo_widget/meteo_journee.py
+++ view/meteo_widget/meteo_journee.py
@@ -2,6 +2,10 @@ from PySide6.QtCore import Slot
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel
 
+from api.day_weather import DayWeather
+from utilitaire.conversion import Conversion
+from utilitaire.load_image_url import LoadImageUrl
+
 
 class MeteoJournee(QWidget):
     def __init__(self, lat, lon, nomville):
@@ -14,38 +18,52 @@ class MeteoJournee(QWidget):
         self.layout_principal.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.layout_principal)
 
-        meteoJourneeLayout = QHBoxLayout()
-
-        for i in range(12):
-            meteoHeure = QVBoxLayout()
-
-            meteoHeureLabel = QLabel(f"{14 + i}h")
-            meteoHeureLabel.setObjectName("meteoHeureLabel")
-            meteoHeure.addWidget(meteoHeureLabel)
-
-            icons = QLabel()
-            pixmap = QPixmap("img.png")
-            icons.setPixmap(pixmap.scaled(50, 50))
-            meteoHeure.addWidget(icons)
-
-            meteoTemp = QLabel(f"{25 + i}°C")
-            meteoTemp.setObjectName("meteoTemp")
-            meteoHeure.addWidget(meteoTemp)
-
-            meteoHum = QLabel(f"Humidité : {60 - i * 5}%")
-            meteoHum.setObjectName("meteoHum")
-            meteoHeure.addWidget(meteoHum)
-
-            meteoJourneeLayout.addLayout(meteoHeure)
+        self.meteoJourneeLayout = QHBoxLayout()
 
         meteoJournee = QWidget()
         meteoJournee.setObjectName("meteo_journee")
-        meteoJournee.setLayout(meteoJourneeLayout)
+        meteoJournee.setLayout(self.meteoJourneeLayout)
 
         self.layout_principal.addWidget(meteoJournee)
 
-    @Slot(str)
-    def set_ville(self, nomville):
-
+    @Slot(float, float, str)
+    def set_ville(self, lat, lon, nomville):
         self.nomville = nomville
-        # appel api
+
+        # Récupère les données météo de la ville
+        weather = DayWeather()
+        dayweather = weather.get_day_weather(lat, lon)
+
+        for time, icon, temp, proba in zip(
+                dayweather["time"],
+                dayweather["icon"],
+                dayweather["temperature_2m"],
+                dayweather["preci_proba"]
+        ):
+            meteoHeure = QVBoxLayout()
+
+            # Heure
+            heure_label = QLabel(str(time))
+            heure_label.setObjectName("meteoHeureLabel")
+            meteoHeure.addWidget(heure_label)
+
+            # Icône
+            image_data = LoadImageUrl().load_image_url(icon)
+            if image_data:
+                pixmap = QPixmap()
+                pixmap.loadFromData(image_data)
+                icons = QLabel("meteo_icon")
+                icons.setPixmap(pixmap.scaled(100, 100))
+                meteoHeure.addWidget(icons)
+
+            # Température
+            temp_label = QLabel(str(temp) + "°C")
+            temp_label.setObjectName("meteoTemp")
+            meteoHeure.addWidget(temp_label)
+
+            # Precipitation probabilité
+            precip_label = QLabel(str(proba) + " %")
+            precip_label.setObjectName("precipProba")
+            meteoHeure.addWidget(precip_label)
+
+            self.meteoJourneeLayout.addLayout(meteoHeure)
