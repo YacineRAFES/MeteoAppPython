8ecac43d2bea7a62753c852cd2fe40e66a8b1654
Yacine RAFES <yacine.rafes@gmail.com>
2026-04-10 10:56:21 +0200

    Ajout d'une méthode statique pour convertir un timestamp en heure au format HH:MM dans conversion.py

--- utilitaire/conversion.py
+++ utilitaire/conversion.py
@@ -5,3 +5,9 @@ class Conversion:
     def from_timestamp_to_datetime(timestamp: int) -> str:
         date = datetime.fromtimestamp(int(timestamp))
         return date.strftime("%A %d %B %Y à %H:%M")
+
+    @staticmethod
+    def from_timestamp_to_hour(timestamp: int) -> str:
+        hour = datetime.fromtimestamp(int(timestamp))
+        return hour.strftime("%H:%M")
+
