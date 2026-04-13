3c0d97eb309a13340ec74f6d48cd5616f73c0cb0
Yacine RAFES <yacine.rafes@gmail.com>
2026-04-13 13:43:55 +0200

    Mettre la méthode en static et enlever le self

--- utilitaire/get_weather_icon.py
+++ utilitaire/get_weather_icon.py
@@ -4,7 +4,8 @@ with open("./assets/weather_code.json") as f:
     WEATHER_DATA = json.load(f)
 
 class weather_icon:
-    def get_weather_icon(self, weather_code, is_day):
+    @staticmethod
+    def get_weather_icon(weather_code, is_day):
         period = "day" if is_day else "night"
         weather = WEATHER_DATA[str(weather_code)][period]
         print(f"Code météo: {weather_code}, Période: {period}, Icon: {weather['image']}, Description: {weather['description']}")
