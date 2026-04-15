1db82652041dabc0744a29e5696aa9ed1d381286
Yacine RAFES <yacine.rafes@gmail.com>
2026-04-15 16:01:47 +0200

    Déplacement du fichier

---
+++ services/geo/geocoding.py
@@ -0,0 +1,23 @@
+import requests
+
+
+
+def get_geo(nomville):
+    url = f"https://geocoding-api.open-meteo.com/v1/search?name={nomville}&count=1&language=en&format=json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "latitude": data["results"][0]["latitude"],
+            "longitude": data["results"][0]["longitude"],
+            "code_country": data["results"][0]["country_code"]
+        }
+    elif response.status_code == 400:
+        data = response.json()
+        return {
+            print(data["error"] + " : " + data["reason"])
+        }
+    else:
+        return {
+            print("Erreur lors de la requête : " + str(response.status_code))
+        }
