43843c532b7539802990e737c9fc84fbc5647428
Yacine RAFES <yacine.rafes@gmail.com>
2026-04-16 15:37:23 +0200

    Modification meteo_semaine.py au niveau l'itération et mise en place pour afficher les données grâce aux variables de modele

--- view/meteo_widget/meteo_semaine.py
+++ view/meteo_widget/meteo_semaine.py
@@ -22,15 +22,17 @@ class MeteoSemaine(QWidget):
         self.meteo_semaine = QHBoxLayout()
         self.meteo_semaine.setObjectName("meteo_semaine")
 
-    @Slot(float, float, str)
-    def set_ville(self, data):
+    def maj_daily(self, daily):
 
-        for daily in data:
+        for i in range(len(daily.times)):
+            icon, desc = daily.get_weather_code(i)
 
             meteo_jour_layout = QVBoxLayout()
             meteo_jour_layout.addStretch()
 
-            meteo_jour_label = QLabel(daily.get)
+            meteo_jour_label = QLabel(
+                f"{daily.get_times(i)}"
+            )
             meteo_jour_label.setAlignment(Qt.AlignCenter)
             meteo_jour_label.setObjectName("meteo_jour_label")
             meteo_jour_layout.addWidget(meteo_jour_label)
@@ -46,17 +48,23 @@ class MeteoSemaine(QWidget):
 
             meteo_temp_layout = QHBoxLayout()
 
-            meteo_temp_min = QLabel(str(temp_min))
+            meteo_temp_min = QLabel(
+                f"{daily.get_temp_min(i)}°C"
+            )
             meteo_temp_min.setObjectName("meteo_temp_min")
             meteo_temp_layout.addWidget(meteo_temp_min)
 
-            meteo_temp_max = QLabel(str(temp_max)+"°C")
+            meteo_temp_max = QLabel(
+                f"{daily.get_temp_max(i)}°C"
+            )
             meteo_temp_max.setObjectName("meteo_temp_max")
             meteo_temp_layout.addWidget(meteo_temp_max)
 
             meteo_jour_layout.addLayout(meteo_temp_layout)
 
-            meteo_preci = QLabel(f"Précip' prob' : " + str(proba) + " %")
+            meteo_preci = QLabel(
+                f"{daily.get_precipitations(i)} %"
+            )
             meteo_preci.setObjectName("meteo_preci")
             meteo_jour_layout.addWidget(meteo_preci)
 
