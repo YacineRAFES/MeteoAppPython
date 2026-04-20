806ac19c256e49f26e9914ee6177cc2d641db33e
Yacine RAFES <yacine.rafes@gmail.com>
2026-04-20 12:08:32 +0200

    Ajout Lock pour mieux gérer les traitements pour chaque ville international et changement d'appel de la méthode pour le géolocalisation

--- utilitaire/geocoding_cache.py
+++ utilitaire/geocoding_cache.py
@@ -1,39 +1,41 @@
 from string import capwords
+from threading import Lock
 import pandas as pd
 from pathlib import Path
 
 from services.geo.geocoding import get_geo
 
 CACHE_PATH = Path(__file__).parent.parent / "cache" / "geocoding.csv"
+_lock = Lock()
 
 def get_geocoding(nomville):
 
-    # Je vérifie si la ville existe dans le geocoding cache
-    if CACHE_PATH.exists() and CACHE_PATH.stat().st_size > 0:
-        print("Lecture du cache de géocodage...")
-        df = pd.read_csv(CACHE_PATH)
-    else:
-        print("Cache de géocodage vide ou inexistant, création d'un nouveau cache...")
-        df = pd.DataFrame(columns=["ville", "code_country", "latitude", "longitude"])
+    with _lock:
+        # Je vérifie si la ville existe dans le geocoding cache
+        if CACHE_PATH.exists() and CACHE_PATH.stat().st_size > 0:
+            print("Lecture du cache de géocodage...")
+            df = pd.read_csv(CACHE_PATH)
+        else:
+            print("Cache de géocodage vide ou inexistant, création d'un nouveau cache...")
+            df = pd.DataFrame(columns=["ville", "code_country", "latitude", "longitude"])
 
-    print("Recherche de la ville dans le cache..." + nomville)
-    result = df[df["ville"] == capwords(nomville)]
+        print("Recherche de la ville dans le cache..." + nomville)
+        result = df[df["ville"] == capwords(nomville)]
 
 
-    # Si la ville n'existe pas, je la rajoute dans le cache
-    if result.empty:
-        print("Ville non trouvée dans le cache, ajout en cours..." + nomville)
-        geo = get_geo(nomville)
-        geocoding = geo.GetGeo(nomville)
-        new_row = {
-            "ville": capwords(nomville),
-            "code_country": geocoding["code_country"],
-            "latitude": geocoding["latitude"],
-            "longitude": geocoding["longitude"]
-        }
-        df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-        df.to_csv(CACHE_PATH, index=False)
-        return new_row
+        # Si la ville n'existe pas, je la rajoute dans le cache
+        if result.empty:
+            print("Ville non trouvée dans le cache, ajout en cours..." + nomville)
+            geocoding = get_geo(nomville)
+            new_row = {
+                "ville": capwords(nomville),
+                "code_country": geocoding["code_country"],
+                "latitude": geocoding["latitude"],
+                "longitude": geocoding["longitude"]
+            }
+            df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+            df.to_csv(CACHE_PATH, index=False)
+            return new_row
 
-    print("Ville trouvée dans le cache : " + nomville)
-    return result.iloc[0].to_dict()
+        print("Ville trouvée dans le cache : " + nomville)
+        return result.iloc[0].to_dict()
