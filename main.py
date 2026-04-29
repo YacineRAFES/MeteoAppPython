4374a8c92303dd51165759d9ccb6c48334cf44be
Yacine RAFES <yacine.rafes@gmail.com>
2026-04-29 15:31:23 +0200

    Modification du main et ajout un bouton accueil

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import locale
 import sys
 
-from PySide6.QtGui import QPalette, QColor
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-from PySide6.QtCore import Qt
+from PySide6.QtGui import QPalette, QColor, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QHBoxLayout, QPushButton
 
 from view.body import RechercherUneVille
 from view.les_villes_internationales import VilleWidget
@@ -18,8 +17,21 @@ class MyWindow(QMainWindow):
         self.setWindowTitle("MeteoApp")
         self.resize(1600, 800)
 
+        # menu de navigation
+        menu_layout = QHBoxLayout()
+        menu_layout.setContentsMargins(0, 0, 0, 10)
+        menu_layout.setSpacing(0)
+
+        bouton_accueil = QPushButton("Accueil")
+        bouton_accueil.setObjectName("boutonMenu")
+        menu_layout.addWidget(bouton_accueil)
+        menu_layout.addStretch()
+
+        menu_widget = QWidget()
+        menu_widget.setLayout(menu_layout)
+
+        # contenu affichage de la page d'accueil
         layout_main = QVBoxLayout()
-        layout_main.setAlignment(Qt.AlignTop)
 
         widget_ville = VilleWidget()
 
@@ -29,8 +41,20 @@ class MyWindow(QMainWindow):
         layout_main.addWidget(body_meteo)
         layout_main.addStretch()
 
+        contenu_widget = QWidget()
+        contenu_widget.setLayout(layout_main)
+
+        # assemblage du menu et du contenu dans la fenêtre principale
+        assemblage_layout = QVBoxLayout()
+        assemblage_layout.setContentsMargins(0, 0, 0, 0)
+        assemblage_layout.setSpacing(0)
+        assemblage_layout.addWidget(menu_widget)
+        assemblage_layout.addWidget(contenu_widget)
+
+        assemblage_layout.addStretch()
+
         central_widget = QWidget()
-        central_widget.setLayout(layout_main)
+        central_widget.setLayout(assemblage_layout)
         self.setCentralWidget(central_widget)
 
 
