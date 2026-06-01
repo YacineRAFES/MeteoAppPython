9a823b82cbe5daf24c4231b83496412ed26f01fc
Yacine RAFES <yacine.rafes@gmail.com>
2026-06-01 11:21:42 +0200

    Ajout un import

--- view/settings/fonctionnalite/les_villes_favoris.py
+++ view/settings/fonctionnalite/les_villes_favoris.py
@@ -1,7 +1,8 @@
 from string import capwords
 
-from PySide6.QtGui import QShortcut, QKeySequence
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QPushButton, QListWidget, QVBoxLayout, QLineEdit
+from PySide6.QtGui import QShortcut, QKeySequence, Qt
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QPushButton, QListWidget, QVBoxLayout, QLineEdit, \
+    QListWidgetItem
 
 from controllers.favorites_city_controller import CityFavouriteController
 from utilitaire.msg_box import message_box, message_box_geocoding
