49aca370a05c8c8de61ed844305c2f6d5f65911f
Yacine RAFES <yacine.rafes@gmail.com>
2026-06-27 16:59:37 +0200

    Suppression les imports inutiles

--- view/accueil/meteo_widget/meteo_journee_charts.py
+++ view/accueil/meteo_widget/meteo_journee_charts.py
@@ -1,6 +1,4 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout
-import numpy as np
-from matplotlib import pyplot as plt
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
