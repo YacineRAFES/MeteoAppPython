0cb2bb1c07113f4f2836b41b1ab9d7a87241bfa4
Yacine RAFES <yacine.rafes@gmail.com>
2026-07-10 11:22:28 +0200

    Mise en place la récupération des données et les afficher sur le graphique

--- view/accueil/meteo_widget/meteo_journee_charts.py
+++ view/accueil/meteo_widget/meteo_journee_charts.py
@@ -1,24 +1,53 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+import matplotlib.patheffects as path_effects
 
 
 class MeteoJourneeCharts(QWidget):
-    def __init__(self):
+    def __init__(self, nomville):
         super().__init__()
+        self.nomville = nomville
+
+        print("MeteoJourneeCharts : ", self.nomville)
+
 
         # Créer un layout pour le graphique
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+        self.meteo_journee_charts = QVBoxLayout()
+        self.setLayout(self.meteo_journee_charts)
 
         self.setMinimumHeight(250)
 
+    def maj_charts(self, data):
 
-        # Créer une figure matplotlib
+        # enregistre les données dans les variables
+        heure_dict = data.get_all_times()
+        temp_dict = data.get_all_temperatures()
+
+        # figure matplotlib graphique pour la température
         fig = Figure()
         ax = fig.subplots(1, 1)
-        ax.plot(["11:00", "14:00", "15:00", "16:00"], [34, 36, 38, 39])
+        min_temp = min(temp_dict) - 4
+        max_temp = max(temp_dict) + 4
+        ax.set_ylim(min_temp, max_temp)
+        for x, y in zip(heure_dict, temp_dict):
+            ax.annotate(str(y),
+                        xy=(x, y),
+                        xytext=(0, 10),
+                        textcoords="offset points",
+                        ha='center',
+                        fontsize=10,
+                        color='black',
+                        path_effects=[
+                            path_effects.withStroke(
+                                linewidth=5,      # Épaisseur du contour
+                                foreground='white' # Couleur du contour
+                            )
+                        ])
+        ax.plot(heure_dict, temp_dict, '.-')
         ax.set_title("Températures aujourd'hui")
+
+
 
         # Créer un canvas pour afficher la figure dans Qt
         canvas = FigureCanvas(fig)
@@ -26,11 +55,14 @@ class MeteoJourneeCharts(QWidget):
         canvas.wheelEvent = self.canvas_wheel_event
 
         # Ajouter le canvas au layout
-        layout.addWidget(canvas)
+        self.meteo_journee_charts.addWidget(canvas)
+
+    def vider(self):
+        while self.meteo_journee_charts.count():
+            item = self.meteo_journee_charts.takeAt(0)
+            widget = item.widget()
+            if widget:
+                widget.deleteLater()
 
     def canvas_wheel_event(self, event):
         event.ignore()
-
-    def maj_charts(self):
-        meteoHeure = QVBoxLayout()
-
