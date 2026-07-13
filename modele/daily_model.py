3ac9c39adcf4eefa4f43f725774f8917187b25af
Yacine RAFES <yacine.rafes@gmail.com>
2026-07-13 22:24:51 +0200

    Modification des variables pour identifier facilement avec les requêtes reçues

--- modele/daily_model.py
+++ modele/daily_model.py
@@ -4,17 +4,17 @@ from utilitaire.get_weather_icon import weather_icon
 
 class WeatherDaily:
     def __init__(self, data):
-        self.temp_min = data["temperature_min"]
-        self.temp_max = data["temperature_max"]
+        self.temperature_2m_min = data["temperature_2m_min"]
+        self.temperature_2m_max = data["temperature_2m_max"]
         self.weather_code = data["weather_code"]
-        self.precipitations = data["preci_proba"]
+        self.precipitation_probability_max = data["precipitation_probability_max"]
         self.day = data["time"]
 
-    def get_temp_min(self, index):
-        return round(self.temp_min[index])
+    def get_temperature_2m_min(self, index):
+        return round(self.temperature_2m_min[index])
 
-    def get_temp_max(self, index):
-        return round(self.temp_max[index])
+    def get_temperature_2m_max(self, index):
+        return round(self.temperature_2m_max[index])
 
     def get_weather_code(self, index):
         weather = weather_icon.get_weather_icon(self.weather_code[index], 1)
@@ -24,8 +24,8 @@ class WeatherDaily:
 
         return icon, description
 
-    def get_precipitations(self, index):
-        return self.precipitations[index]
+    def get_precipitation_probability_max(self, index):
+        return self.precipitation_probability_max[index]
 
     def get_day(self, index):
         return Conversion.from_timestamp_to_day(self.day[index])
