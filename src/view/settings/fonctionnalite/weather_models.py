f12983b6d12026c4d60b6589853f7a391514381f
yacine <yacine.rafes@gmail.com>
2026-07-29 14:42:29 +0200

    mise en place le chemin de modele de météo

--- src/view/settings/fonctionnalite/weather_models.py
+++ src/view/settings/fonctionnalite/weather_models.py
@@ -1,10 +1,17 @@
+import json
+
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QVBoxLayout, QRadioButton
+
+from src.resources import resource_path
+
+with resource_path("view","settings","fonctionnalite","weather_models.json").open(encoding="utf-8") as f:
+    MODELS_DATA = json.load(f)
 
 class WeatherModels(QWidget):
     def __init__(self, header_instance=None):
         super().__init__()
         self.header_instance = header_instance
-
+        print(MODELS_DATA)
         self.layout = QVBoxLayout()
         self.layout.addStretch()
 
@@ -13,6 +20,7 @@ class WeatherModels(QWidget):
         self.layout.addWidget(self.titre)
 
         self.layout_add_city = QHBoxLayout()
+
 
         self.button_radio = QRadioButton("test")
         self.button_radio = QRadioButton("test")
