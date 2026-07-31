bbc2336dc7d4fb06d0e5404674405a34b417217a
yacine <yacine.rafes@gmail.com>
2026-07-31 10:33:31 +0200

    Ajout un scrolling sur la page de paramètre

--- src/view/settings/parametre.py
+++ src/view/settings/parametre.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QScrollArea
 
 from src.view.settings.fonctionnalite.les_villes_favoris import FavouriteCity
 from src.view.settings.fonctionnalite.weather_models import WeatherModels
@@ -8,28 +8,36 @@ class Parametre(QWidget):
     def __init__(self, main_header, parent=None):
         super().__init__(parent)
 
-        layout_main = QVBoxLayout()
+        layout_principal = QVBoxLayout()
+        layout_principal.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout_principal)
+
+        # Widget conteneur qui ira DANS la scrollarea
+        contenu = QWidget()
+        layout_contenu = QVBoxLayout()
+        contenu.setLayout(layout_contenu)
+
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setWidget(contenu)  # <-- l'étape qui manquait
+        layout_principal.addWidget(self.scroll_area)
+
         layout_ligne_un = QHBoxLayout()
         layout_ligne_deux = QHBoxLayout()
 
-        # Ajoutez ici les éléments de votre page de paramètres
         titre_parametres = QLabel("Paramètres")
         titre_parametres.setObjectName("titreParametres")
-        layout_main.addWidget(titre_parametres)
+        layout_contenu.addWidget(titre_parametres)  # <-- ajouté au contenu, pas au layout_principal
 
         les_villes_favoris = FavouriteCity(header_instance=main_header)
         weather_models = WeatherModels(header_instance=main_header)
-        # Ajout les paramètres pour °C et °F
-
 
         layout_ligne_un.addWidget(les_villes_favoris)
         layout_ligne_un.addStretch()
 
         layout_ligne_deux.addWidget(weather_models)
 
-        layout_main.addLayout(layout_ligne_un)
-        layout_main.addLayout(layout_ligne_deux)
+        layout_contenu.addLayout(layout_ligne_un)
+        layout_contenu.addLayout(layout_ligne_deux)
 
-        layout_main.addStretch()
-
-        self.setLayout(layout_main)
+        layout_contenu.addStretch()
